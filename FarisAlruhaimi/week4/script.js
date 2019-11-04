99dac99e7f754eaecbfc8c27178185e882e56ed4
fariscc <fariscc@gmail.com>
2019-11-04 11:48:42 +0300

    Homework Week4
    comfortable: 4/5
    completeness: 5/5
    Notes:

---
+++ FarisAlruhaimi/week4/script.js
@@ -0,0 +1,18 @@
+console.log("JS connected");
+
+var body = document.querySelector('body');
+
+body.style.fontFamily = "Arial, sans-serif";
+
+body.style.backgroundColor = "blue";
+body.style.color = "white";
+
+
+var nickname = document.getElementById("nickname");
+var favorites = document.getElementById("favorites");
+var homwtown = document.getElementById("hometown");
+
+
+nickname.innerHTML = "Faris Alruhaimi";
+favorites.innerHTML = "Programming, Reading books, Playing soccer";
+homwtown.innerHTML = "Madinah";
